f8ff304fe8484e656de8f34ffe5ecfdb185a341b
haruww <michael1302@naver.com>
2022-12-07 21:43:47 +0900

    0.2 Update

--- src/main/java/me/haru301/megplus/EventHandle.java
+++ src/main/java/me/haru301/megplus/EventHandle.java
@@ -1,12 +1,20 @@
 package me.haru301.megplus;
 
+import com.ticxo.modelengine.api.ModelEngineAPI;
+import com.ticxo.modelengine.api.animation.AnimationHandler;
+import com.ticxo.modelengine.api.model.ActiveModel;
+import com.ticxo.modelengine.api.model.ModeledEntity;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
+import java.util.Map;
 import java.util.UUID;
 
 public class EventHandle implements Listener
@@ -14,9 +22,40 @@ public class EventHandle implements Listener
     @EventHandler
     public void onRightClick(PlayerInteractEntityEvent e)
     {
-        Player p = e.getPlayer();
         Entity entity = e.getRightClicked();
         UUID uuid = entity.getUniqueId();
         Bukkit.getLogger().info("uuid: " + uuid);
+    }
+
+    @EventHandler
+    public void onRightClickArmorStand(PlayerInteractAtEntityEvent e)
+    {
+        Entity entity = e.getRightClicked();
+        if(entity instanceof ArmorStand)
+        {
+            UUID uuid = entity.getUniqueId();
+            Bukkit.getLogger().info("uuid: " + uuid);
+        }
+    }
+
+    @EventHandler
+    public void onEntityAttackEntity(EntityDamageByEntityEvent e)
+    {
+        Entity attacker = e.getDamager();
+        UUID targetUUID = attacker.getUniqueId();
+        ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(targetUUID);
+        if(modeledEntity == null)
+            return;
+
+        Map<String, ActiveModel> model = modeledEntity.getModels();
+        ActiveModel activeModel = null;
+        for(String strKey : model.keySet())
+            activeModel = model.get(strKey);
+
+        AnimationHandler animationHandler = activeModel.getAnimationHandler();
+        if(animationHandler == null)
+            return;
+
+        animationHandler.playAnimation("attack", 0, 0, 1, true);
     }
 }
--- src/main/java/me/haru301/megplus/MegPlus.java
+++ src/main/java/me/haru301/megplus/MegPlus.java
@@ -5,16 +5,14 @@ import com.ticxo.modelengine.api.animation.AnimationHandler;
 import com.ticxo.modelengine.api.model.ActiveModel;
 import com.ticxo.modelengine.api.model.ModeledEntity;
 import dev.jorel.commandapi.CommandAPICommand;
-import dev.jorel.commandapi.arguments.LiteralArgument;
-import dev.jorel.commandapi.arguments.StringArgument;
-import dev.jorel.commandapi.arguments.UUIDArgument;
+import dev.jorel.commandapi.arguments.*;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
 
@@ -27,168 +25,179 @@ public final class MegPlus extends JavaPlugin
 
         new CommandAPICommand("megplus")
                 .withArguments(new LiteralArgument("motionlist"))
-                .withArguments(new UUIDArgument("target UUID"))
+                .withArguments(new EntitySelectorArgument<Collection<Entity>>("target Entity", EntitySelector.MANY_ENTITIES))
                 .executesPlayer(this::motionlist)
                 .register();
 
         new CommandAPICommand("megplus")
                 .withArguments(new LiteralArgument("disguise"))
                 .withArguments(new StringArgument("Model ID"))
-                .withArguments(new UUIDArgument("target UUID"))
+                .withArguments(new EntitySelectorArgument<Collection<Entity>>("target Entity", EntitySelector.MANY_ENTITIES))
                 .executes((this::disguise))
                 .register();
 
         new CommandAPICommand("megplus")
                 .withArguments(new LiteralArgument("undisguise"))
-                .withArguments(new UUIDArgument("target UUID"))
+                .withArguments(new EntitySelectorArgument<Collection<Entity>>("target Entity", EntitySelector.MANY_ENTITIES))
                 .executes((this::undisguise))
                 .register();
 
         new CommandAPICommand("megplus")
                 .withArguments(new LiteralArgument("motion"))
                 .withArguments(new StringArgument("Motion ID"))
-                .withArguments(new UUIDArgument("target UUID"))
+                .withArguments(new EntitySelectorArgument<Collection<Entity>>("target Entity", EntitySelector.MANY_ENTITIES))
                 .executes((this::motion))
                 .register();
 
         new CommandAPICommand("megplus")
                 .withArguments(new LiteralArgument("nomotion"))
-                .withArguments(new UUIDArgument("target UUID"))
+                .withArguments(new EntitySelectorArgument<Collection<Entity>>("target Entity", EntitySelector.MANY_ENTITIES))
                 .executes((this::nomotion))
                 .register();
     }
 
     private void motionlist(CommandSender sender, Object[] args)
     {
-        UUID targetUUID = (UUID) args[0];
+        Collection<Entity> entities = (Collection<Entity>) args[0];
 
-        ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(targetUUID);
-        if(modeledEntity == null)
+        for(Entity e : entities)
         {
-            sender.sendMessage(ChatColor.RED + "대상에게 모델이 입혀져있지 않습니다. (또는 다른 이유)");
-            return;
-        }
+            UUID targetUUID = e.getUniqueId();
+            ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(targetUUID);
+            if(modeledEntity == null)
+            {
+                sender.sendMessage(ChatColor.YELLOW + targetUUID.toString());
+                sender.sendMessage(ChatColor.RED + "대상에게 모델이 입혀져있지 않습니다. (또는 다른 이유)");
+                sender.sendMessage(" ");
+                return;
+            }
 
-        Map<String, ActiveModel> model = modeledEntity.getModels();
-        ActiveModel activeModel = null;
-        for(String strKey : model.keySet())
-            activeModel = model.get(strKey);
+            Map<String, ActiveModel> model = modeledEntity.getModels();
+            ActiveModel activeModel = null;
+            for(String strKey : model.keySet())
+                activeModel = model.get(strKey);
 
-        for(String keySet : activeModel.getBlueprint().getAnimations().keySet())
-        {
-            sender.sendMessage(activeModel.getBlueprint().getAnimations().get(keySet).getName());
-
+            sender.sendMessage(ChatColor.YELLOW + targetUUID.toString());
+            for(String keySet : activeModel.getBlueprint().getAnimations().keySet())
+            {
+                sender.sendMessage(activeModel.getBlueprint().getAnimations().get(keySet).getName());
+            }
+            sender.sendMessage(" ");
         }
     }
 
     private void disguise(CommandSender sender, Object[] args)
     {
         String modelName = (String) args[0];
-        UUID targetUUID = (UUID) args[1];
+        Collection<Entity> entities = (Collection<Entity>) args[1];
 
-        Entity targetEntity = getEntityByUniqueId(targetUUID);
-        if(targetEntity == null)
+        if(entities == null)
         {
-            sender.sendMessage(ChatColor.RED + "엔티티를 찾을 수 없습니다. : " + targetUUID);
+            sender.sendMessage(ChatColor.RED + "엔티티를 찾을 수 없습니다. : ");
             return;
         }
 
-        ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(targetUUID);
-        if(modeledEntity == null)
+        for(Entity e : entities)
         {
-            modeledEntity = ModelEngineAPI.createModeledEntity(targetEntity);
-            modeledEntity.setBaseEntityVisible(false);
-        }
-        else
-        {
-            sender.sendMessage(ChatColor.RED + "모델이 이미 적용되어있습니다. undisguise를 먼저 해주세요.");
-            return;
-        }
+            UUID targetUUID = e.getUniqueId();
+            ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(targetUUID);
+            if(modeledEntity == null)
+            {
+                modeledEntity = ModelEngineAPI.createModeledEntity(e);
+                modeledEntity.setBaseEntityVisible(false);
+            }
+            else
+            {
+                sender.sendMessage(ChatColor.RED + "모델이 이미 적용되어있습니다. undisguise를 먼저 해주세요.");
+                continue;
+            }
 
-        ActiveModel model = ModelEngineAPI.createActiveModel(modelName);
-        if(model == null)
-        {
-            sender.sendMessage(ChatColor.RED + "모델을 불러올 수 없습니다. : " + modelName);
-            return;
-        }
+            ActiveModel model = ModelEngineAPI.createActiveModel(modelName);
+            if(model == null)
+            {
+                sender.sendMessage(ChatColor.RED + "모델을 불러올 수 없습니다. : " + modelName);
+                return;
+            }
 
-        modeledEntity.addModel(model, true);
+            modeledEntity.addModel(model, true);
+        }
     }
 
     private void undisguise(CommandSender sender, Object[] args)
     {
-        UUID targetUUID = (UUID) args[0];
+        Collection<Entity> entities = (Collection<Entity>) args[0];
 
-        ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(targetUUID);
-        if(modeledEntity == null)
-            return;
-        modeledEntity.setBaseEntityVisible(true);
-        ModelEngineAPI.removeModeledEntity(targetUUID);
+        for(Entity e : entities)
+        {
+            UUID targetUUID = e.getUniqueId();
+            ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(targetUUID);
+            if(modeledEntity == null)
+                continue;
+            modeledEntity.setBaseEntityVisible(true);
+            ModelEngineAPI.removeModeledEntity(targetUUID);
+        }
     }
 
     private void motion(CommandSender sender, Object[] args)
     {
         String motionName = (String) args[0];
-        UUID targetUUID = (UUID) args[1];
+        Collection<Entity> entities = (Collection<Entity>) args[1];
 
-        ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(targetUUID);
-        if(modeledEntity == null)
+        for(Entity e : entities)
         {
-            sender.sendMessage(ChatColor.RED + "대상에게 모델이 입혀져있지 않습니다. (또는 다른 이유)");
-            return;
+            UUID targetUUID = e.getUniqueId();
+
+            ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(targetUUID);
+            if(modeledEntity == null)
+            {
+                sender.sendMessage(ChatColor.RED + "대상에게 모델이 입혀져있지 않습니다. (또는 다른 이유)");
+                continue;
+            }
+
+            Map<String, ActiveModel> model = modeledEntity.getModels();
+            ActiveModel activeModel = null;
+            for(String strKey : model.keySet())
+                activeModel = model.get(strKey);
+
+            AnimationHandler animationHandler = activeModel.getAnimationHandler();
+            if(animationHandler == null)
+            {
+                sender.sendMessage(ChatColor.RED + "AnimationHandler를 불러올 수 없습니다.");
+                continue;
+            }
+
+            if(!animationHandler.playAnimation(motionName, 0, 0, 1, true))
+                sender.sendMessage(ChatColor.RED + "애니메이션이 존재하지 않습니다.");
         }
-
-        Map<String, ActiveModel> model = modeledEntity.getModels();
-        ActiveModel activeModel = null;
-        for(String strKey : model.keySet())
-            activeModel = model.get(strKey);
-
-        AnimationHandler animationHandler = activeModel.getAnimationHandler();
-        if(animationHandler == null)
-        {
-            sender.sendMessage(ChatColor.RED + "AnimationHandler를 불러올 수 없습니다.");
-            return;
-        }
-
-        if(!animationHandler.playAnimation(motionName, 0, 0, 1, true))
-            sender.sendMessage(ChatColor.RED + "애니메이션이 존재하지 않습니다.");
     }
 
     private void nomotion(CommandSender sender, Object[] args)
     {
-        UUID targetUUID = (UUID) args[0];
+        Collection<Entity> entities = (Collection<Entity>) args[0];
 
-        ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(targetUUID);
-        if(modeledEntity == null)
+        for(Entity e : entities)
         {
-            sender.sendMessage(ChatColor.RED + "대상에게 모델이 입혀져있지 않습니다. (또는 다른 이유)");
-            return;
-        }
+            UUID targetUUID = e.getUniqueId();
 
-        Map<String, ActiveModel> model = modeledEntity.getModels();
-        ActiveModel activeModel = null;
-        for(String strKey : model.keySet())
-            activeModel = model.get(strKey);
-
-        AnimationHandler animationHandler = activeModel.getAnimationHandler();
-        if(animationHandler == null)
-        {
-            sender.sendMessage(ChatColor.RED + "AnimationHandler를 불러올 수 없습니다.");
-            return;
-        }
-        animationHandler.forceStopAllAnimations();
-    }
-
-    private Entity getEntityByUniqueId(UUID uniqueId)
-    {
-        for (World world : Bukkit.getWorlds())
-        {
-            for (Entity entity : world.getEntities())
+            ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(targetUUID);
+            if(modeledEntity == null)
             {
-                if (entity.getUniqueId().equals(uniqueId))
-                    return entity;
+                sender.sendMessage(ChatColor.RED + "대상에게 모델이 입혀져있지 않습니다. (또는 다른 이유)");
+                continue;
             }
+
+            Map<String, ActiveModel> model = modeledEntity.getModels();
+            ActiveModel activeModel = null;
+            for(String strKey : model.keySet())
+                activeModel = model.get(strKey);
+
+            AnimationHandler animationHandler = activeModel.getAnimationHandler();
+            if(animationHandler == null)
+            {
+                sender.sendMessage(ChatColor.RED + "AnimationHandler를 불러올 수 없습니다.");
+                continue;
+            }
+            animationHandler.forceStopAllAnimations();
         }
-        return null;
     }
 }
